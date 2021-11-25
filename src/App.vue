9184d207a5e39c05e6beea6eb763327c3c0bc40d
pollohultima <matteo.polonio@gmail.com>
2021-11-25 10:54:44 +0100

    Add 3 main components Add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <SiteHeader />
+    <SiteMain />
+    <SiteFooter />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import SiteHeader from "./components/SiteHeader.vue";
+import SiteMain from "./components/SiteMain.vue";
+import SiteFooter from "./components/SiteFooter.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    SiteHeader,
+    SiteMain,
+    SiteFooter,
   },
 };
 </script>
@@ -19,10 +24,5 @@ export default {
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
