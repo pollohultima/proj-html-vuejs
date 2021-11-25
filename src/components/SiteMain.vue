32c1e7ecd65aa11f34b40d4a059f66e4c40758e2
pollohultima <matteo.polonio@gmail.com>
2021-11-25 14:57:05 +0100

    Basic siteMain structure done

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,5 +1,38 @@
 <template>
-  <main></main>
+  <main>
+    <section class="coding_course">
+      <div class="container">
+        <h6></h6>
+        <h1></h1>
+
+        <div class="coding_features">
+          <div class="row">
+            <div class="col-3"></div>
+            <div class="col-3"></div>
+            <div class="col-3"></div>
+            <div class="col-3"></div>
+          </div>
+        </div>
+
+        <div class="maxcoach">
+          <img src="" alt="" />
+          <div class="sidetext"></div>
+        </div>
+      </div>
+    </section>
+
+    <div class="data_banner">
+      <div class="container"></div>
+    </div>
+
+    <section class="last_courses"></section>
+
+    <section class="e_learn"></section>
+
+    <section class="events"></section>
+
+    <section class="feedback"></section>
+  </main>
 </template>
 
 <script>
